3ca010d51e664f8de36a4a66af7d74a0c5bda701
Hayk Hovhannisyan <veloxpro@gmail.com>
2013-04-09 15:59:21 +0200

    menu recovery

--- pages/mainMenu.php
+++ pages/mainMenu.php
@@ -1,12 +1,12 @@
 <div id="tabs">
 	<ul>
-		<li class="tab <?php if ($pageName == 'servers') echo 'tabselected' ?>">
+		<li class="tab <?php if ($pageName == 'servers') echo 'tabselected'; ?>">
 			<a href="<?php echo MONITIS_APP_URL ?>&monitis_page=servers">Servers</a>
 		</li>
-		<li class="tab <?php if ($pageName == 'configure') echo 'tabselected' ?>">
+		<li class="tab  <?php if ($pageName == 'configure') echo 'tabselected'; ?>">
 			<a href="<?php echo MONITIS_APP_URL ?>&monitis_page=configure">Settings</a>
 		</li>
-		<li class="tab <?php if ($pageName == 'monitisAccount') echo 'tabselected' ?>">
+		<li class="tab  <?php if ($pageName == 'monitisAccount') echo 'tabselected'; ?>">
 			<a href="<?php echo MONITIS_APP_URL ?>&monitis_page=monitisAccount">Monitis Account</a>
 		</li>
 	</ul>
